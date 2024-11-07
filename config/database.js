2f76b7a90995361a5d9fb1cb0eb5b4fdb0ee833c
krupalsinhchavda <krupalsinh.chavda.epistic@gmail.com>
2024-11-07 18:22:56 +0530

    changes

--- config/database.js
+++ config/database.js
@@ -1,34 +1,12 @@
-require('dotenv').config();
-
-const mysql = require('mysql');
-const dbconnection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'stock'
-});
-dbconnection.connect(function (err) {
-    if (err) {
-        console.log(err);
-    }
-    else {
-        console.log("Connected to database!");
-    }
-});
-
-module.exports = dbconnection;
-
 // require('dotenv').config();
 
 // const mysql = require('mysql');
 // const dbconnection = mysql.createConnection({
-//     host: 'sql12.freesqldatabase.com',
-//     user: 'sql12743226',
-//     password: 'xcCzRVcIga',
-//     database: 'sql12743226',
-//     port: 3306
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'stock'
 // });
-
 // dbconnection.connect(function (err) {
 //     if (err) {
 //         console.log(err);
@@ -39,3 +17,25 @@ module.exports = dbconnection;
 // });
 
 // module.exports = dbconnection;
+
+require('dotenv').config();
+
+const mysql = require('mysql');
+const dbconnection = mysql.createConnection({
+    host: 'sql12.freesqldatabase.com',
+    user: 'sql12743226',
+    password: 'xcCzRVcIga',
+    database: 'sql12743226',
+    // port: 3306
+});
+
+dbconnection.connect(function (err) {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log("Connected to database!");
+    }
+});
+
+module.exports = dbconnection;
